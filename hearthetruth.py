869cbc23f0f810719c858e63b652695347c853c8
M4GE <kayrafickdich@gmail.com>
2024-10-13 09:08:41 +0200

    Add files via upload
    anl ittle project i made on python just for fun.

---
+++ hearthetruth.py
@@ -0,0 +1,12 @@
+print("enter ur weight to hear the truth")
+print ("in kgs pls")
+weight = float(input("weight? "))
+if weight > 60:
+    print("eat less")
+    print("fat fuck")
+elif weight < 60:
+    print("skinny fuck.")
+    print("except ur an girl then hit me up,DC: th3r3alm4ge ;3")
+else:
+    print("good boy")
+input()
